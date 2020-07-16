b2dc8b25b2220704932be6a3cc48a0a98c1bb47e
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-16 17:07:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    
+    erb :user_input
+    
+  end 
+  
+  post 'piglatinize' do
+    
+    pl = Piglatinizer.new 
+    @piglatin = pl.piglatinizer(params[:user_phrase])
+    erb :results 
+    
+  end 
+  
+  
 end
